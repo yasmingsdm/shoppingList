403daae673978518774ee28557399895a7217bfd
Yasmin Dal Medico <yasmin.dm@gmail.com>
2022-11-29 15:07:52 +0100

    apagar e riscar

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function colocarNaLista(){
     let prioridade = document.getElementById('prioridade').value
 
     //Prioridades:
-    console.log(prioridade)
+    
     if (prioridade >5){
         item = item.toUpperCase()
     }
@@ -44,18 +44,49 @@ function colocarNaLista(){
         document.getElementById('lista-oob').innerHTML += '<li onClick="item1()">' + item + '</li>'
     } else if (onde == 'normal'){
         itemNormal.push(item)
-        document.getElementById('lista-normal').innerHTML = itemNormal
+        document.getElementById('lista-normal').innerHTML += '<li onClick="item2()">' + item + '</li>'
     } else if (onde == 'mercado'){
         itemMercado.push(item)
-        document.getElementById('lista-mercado').innerHTML = itemMercado
+        document.getElementById('lista-mercado').innerHTML += '<li onClick="item3()">' + item + '</li>'
     }else{
         itemOutro.push(item)
-        document.getElementById('lista-outro').innerHTML = itemOutro
+        document.getElementById('lista-outro').innerHTML += '<li onClick="item4()">' + item + '</li>'
     }
 
 
 }
 
+//Riscar a lista:
+
 const item1 = () => {
-    console.log ('yeah')
+    document.getElementById('lista-oob').style.textDecoration = 'line-through'
+}
+
+const item2 = () => {
+    document.getElementById('lista-normal').style.textDecoration = 'line-through'
+}
+
+const item3 = () => {
+    document.getElementById('lista-mercado').style.textDecoration = 'line-through'
+}
+
+const item4 = () => {
+    document.getElementById('lista-outro').style.textDecoration = 'line-through'
+}
+
+//Apagar lista:
+const doneMercado = ()=>{
+    document.getElementById('lista-mercado').remove()
+}
+
+const doneOob = ()=>{
+    document.getElementById('lista-oob').remove()
+}
+
+const doneNormal = ()=>{
+    document.getElementById('lista-normal').remove()
+}
+
+const doneOutro = ()=>{
+    document.getElementById('lista-outro').remove()
 }
